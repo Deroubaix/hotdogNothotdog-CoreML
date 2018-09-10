5a6a24803c874153e53632794d1a2880d95310d0
Marisha <marishaderoubaix@marishas-mbp.lan>
2018-09-10 21:02:37 +0100

    Initial Commit

---
+++ hotdogNothotdog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hotdogNothotdog
+//
+//  Created by Marisha Deroubaix on 10/09/18.
+//  Copyright © 2018 Marisha Deroubaix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
